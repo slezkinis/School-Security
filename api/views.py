ec26414988a9e16754a0ef28e3f3d2373102a39a
slezkinis <slezkin.is@mail.ru>
2023-09-10 18:21:30 +0300

    Update

--- api/views.py
+++ api/views.py
@@ -13,6 +13,7 @@ from django.core.files.base import ContentFile
 from django.utils import timezone
 import warnings
 from api.management.commands.bot import process
+import time
 
 
 warnings.simplefilter("ignore")
@@ -24,8 +25,9 @@ def api_enter(request):
         return redirect('/')
     if 'Authorization' in request.headers:
         if request.headers['Authorization'] == '1234':
-            path = f'media/test/enter/{request.data["filename"]}'
-            with default_storage.open(f'test/enter/{request.data["filename"]}', 'wb+') as destination:
+            start_time = time.thread_time_ns()
+            path = f'media/test/enter/{start_time}.png'
+            with default_storage.open(f'test/enter/{start_time}.png', 'wb+') as destination:
                 for chunk in request.data['file'].chunks():
                     destination.write(chunk)
             unknown_image = face_recognition.load_image_file(path)
@@ -38,7 +40,7 @@ def api_enter(request):
                 known_image = face_recognition.load_image_file(i.picture.path)
                 known_encoding = face_recognition.face_encodings(known_image)[0]
                 if face_recognition.compare_faces([known_encoding], unknown_encoding)[0]:
-                    if (timezone.now().day - i.last_exit.day) > 0 or (timezone.now().hour - i.last_exit.hour) > 0 or ((timezone.now().minute - i.last_exit.minute) >= 1) or (timezone.now().minute - i.last_enter.minute) >= 1:
+                    if (timezone.now().day - i.last_exit.day) > 0 or (timezone.now().hour - i.last_exit.hour) > 0 or ((timezone.now().minute - i.last_exit.minute) >= 1):
                         if i.is_enter == False:
                             i.is_enter = True
                             i.last_enter = datetime.datetime.now()
@@ -94,8 +96,9 @@ def api_exit(request):
         return redirect('/')
     if 'Authorization' in request.headers:
         if request.headers['Authorization'] == '1234':
-            path = f'media/test/exit/{request.data["filename"]}'
-            with default_storage.open(f'test/exit/{request.data["filename"]}', 'wb+') as destination:
+            start_time = time.thread_time_ns()
+            path = f'media/test/exit/{start_time}.png'
+            with default_storage.open(f'test/exit/{start_time}.png', 'wb+') as destination:
                 for chunk in request.data['file'].chunks():
                     destination.write(chunk)
             unknown_image = face_recognition.load_image_file(path)
@@ -108,7 +111,7 @@ def api_exit(request):
                 known_image = face_recognition.load_image_file(i.picture.path)
                 known_encoding = face_recognition.face_encodings(known_image)[0]
                 if face_recognition.compare_faces([known_encoding], unknown_encoding)[0]:
-                    if (timezone.now().day - i.last_enter.day) > 0 or (timezone.now().hour - i.last_enter.hour) > 0 or ((timezone.now().minute - i.last_enter.minute) >= 1) or (timezone.now().minute - i.last_exit.minute) >= 1:
+                    if (timezone.now().day - i.last_enter.day) > 0 or (timezone.now().hour - i.last_enter.hour) > 0 or ((timezone.now().minute - i.last_enter.minute) >= 1):
                         if i.is_enter == True:
                             i.is_enter = False
                             i.last_exit = datetime.datetime.now()
@@ -128,7 +131,7 @@ def api_exit(request):
                     known_image = face_recognition.load_image_file(i.picture.path)
                     known_encoding = face_recognition.face_encodings(known_image)[0]
                     if face_recognition.compare_faces([known_encoding], unknown_encoding)[0]:
-                        if (timezone.now().hour - i.last_enter.hour) > 1 or ((timezone.now().minute - i.last_enter.minute) >= 1) or (timezone.now().minute - i.last_exit.minute) >= 1:
+                        if (timezone.now().hour - i.last_enter.hour) > 1 or ((timezone.now().minute - i.last_enter.minute) >= 1) :
                             os.remove(i.picture.path)
                             b = i.id
                             UnknownEnterPerson.objects.filter(id=i.id).delete()
